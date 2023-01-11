c78443d67f2309bf8f893bea136a33fa8feefe51
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-11 14:59:13 +0200

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+	fontSizes: [12, 14, 16, 20, 24, 32],
+
+	fontWeights: {
+		normal: 400,
+		semiBold: 500,
+		bold: 600,
+		extraBold: 700,
+	},
+
+	colors: {
+		blue: '#07c',
+		green: '#0fa',
+	},
+
+	space: 4,
+};
