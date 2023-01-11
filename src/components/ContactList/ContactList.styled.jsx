3b057023ad38e035026a964dffaf2a48c1943cb2
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-11 21:27:42 +0200

    Create ContactList.styled.jsx

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const List = styled.ul``;
+
+export const ContactName = styled.p``;
+
+export const PhoneNumber = styled.p``;
+
+export const DeleteBtn = styled.button``;
