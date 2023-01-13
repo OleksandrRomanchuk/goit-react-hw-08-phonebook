4036924889d709c79e1196272267edc37f30582f
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-13 09:37:01 +0200

    Create Notification.jsx

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import { NotifyMessage } from './Notification.styled';
+
+export function Notification({message}) {
+    return <NotifyMessage>
+                {message}
+            </NotifyMessage>;
+};
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+}
