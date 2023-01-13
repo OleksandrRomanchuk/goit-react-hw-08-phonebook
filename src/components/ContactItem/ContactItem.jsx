5cf582d04bc3c45c8d4089be98e44547e3591ff8
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-13 20:02:58 +0200

    Create ContactItem.jsx

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { TiUserDeleteOutline } from 'react-icons/ti';
+import {ContactWrapper, ContactName, PhoneNumber, DeleteBtn} from './ContactItem.styled'
+
+export function ContactItem({ contact: {name, number}, deleteContact, allyProps }) {
+    return <>
+        <ContactWrapper>
+            <ContactName>{name}</ContactName>
+            <PhoneNumber>{number}</PhoneNumber>
+        </ContactWrapper>
+                
+        <DeleteBtn
+            type="button"
+            onClick={deleteContact}
+            title="Delete contact"
+            {...allyProps}
+        ><TiUserDeleteOutline />
+        </DeleteBtn>
+    </>;
+};
+
+ContactItem.propTypes = {
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+    deleteContact: PropTypes.func.isRequired,
+}
