25b25cea2624f6de4486c82e143a7e6288357713
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-13 09:40:07 +0200

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
-export const AppSection = styled.section``;
+export const AppSection = styled.section`
+    min-width: 210px;
+    max-width: 210px;
+    
+    padding-top: 20px;
+`;
 
-export const SectionTitle = styled.h2``;
+export const SectionTitle = styled.h2`
+    position: relative;
+
+    ::after {
+        content: '';
+        position: absolute;
+        left: -10px;
+        bottom: -6px;
+
+        display: block;
+
+        width: calc(100% + 20px);
+        height: 1px;
+
+        background-color: #ffffff66;
+    }
+`;
