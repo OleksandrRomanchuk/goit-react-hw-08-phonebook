21001d825b3f4ea211f979bf343fdea474f2eb0f
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-13 09:38:29 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types';
-import { List, ContactName, PhoneNumber, DeleteBtn } from './ContactList.styled';
+import { TiUserDeleteOutline } from 'react-icons/ti';
+import { List, ListItem, ContactWrapper, ContactName, PhoneNumber, DeleteBtn } from './ContactList.styled';
 
 export function ContactList({deleteContact, contacts}) {
     return <List>
         {contacts().map(({ id, name, number }) => {
-            return <li key={ id } data-id={id}>
-                <ContactName>{ name }</ContactName>
-                <PhoneNumber>{number}</PhoneNumber>
-                <DeleteBtn type="button" onClick={deleteContact}>Delete</DeleteBtn>
-                    </li>
+            return <ListItem key={id} data-id={id}>
+                <ContactWrapper>
+                    <ContactName>{ name }</ContactName>
+                    <PhoneNumber>{number}</PhoneNumber>
+                </ContactWrapper>
+                
+                <DeleteBtn
+                    type="button"
+                    onClick={deleteContact}
+                    title="Delete contact"
+                    ><TiUserDeleteOutline/>
+                </DeleteBtn>
+                    </ListItem>
                 })}
             </List>;
 };
@@ -16,9 +25,4 @@ export function ContactList({deleteContact, contacts}) {
 ContactList.propTypes = {
     deleteContact: PropTypes.func.isRequired,
     contacts: PropTypes.func.isRequired,
-    // contacts: PropTypes.arrayOf(PropTypes.shape({
-    //     id: PropTypes.string.isRequired,
-    //     name: PropTypes.string.isRequired,
-    //     number: PropTypes.string.isRequired,
-    // })),
 }
