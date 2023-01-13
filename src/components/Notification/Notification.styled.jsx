e9db975880dc548f1c2cdb63a66371fe34c5ed06
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-13 09:39:49 +0200

    Create Notification.styled.jsx

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NotifyMessage = styled.p`
+    min-height: 200px;
+    max-height: 200px;
+
+    margin-top: 20px;
+    padding-top: 30px;
+
+    font-size: 12px;
+    color: #ffffff66;
+`;
