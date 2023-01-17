d04e282b1b9fe08d9a81c33a54e791f261a19375
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-17 20:28:58 +0200

    Update Notification.styled.jsx

--- src/components/Notification/Notification.styled.jsx
+++ src/components/Notification/Notification.styled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const NotifyMessage = styled.p`
-    min-height: 205px;
-    max-height: 205px;
+    min-height: 200px;
+    max-height: 200px;
 
     margin-top: 20px;
     padding-top: 30px;
