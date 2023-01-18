90b47e28dbd9f07a6044beeb08247466f573e2f9
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-18 08:59:27 +0200

    Create localStfunc.js

---
+++ src/helpers/localStfunc.js
@@ -0,0 +1,19 @@
+const saveDataToLocalSt = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const loadDataFromLocalSt = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export { saveDataToLocalSt, loadDataFromLocalSt };
