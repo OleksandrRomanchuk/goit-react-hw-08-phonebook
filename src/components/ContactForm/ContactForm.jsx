cc5cfe09e1ab813cd9d142e5dbe6cec61feab43a
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-18 10:58:22 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -41,33 +41,33 @@ class ContactForm extends Component {
 
     render() {
         return <Form onSubmit={this.onFormSubmit}>
-        <Label>
-            Name
-            <Input
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-            /></Label>
+            <Label>
+                Name
+                <Input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                /></Label>
             
-        <Label>
-            Number
-            <Input
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-            /></Label>
+            <Label>
+                Number
+                <Input
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                /></Label>
             
-        <SubmitBtn
-            type="submit">
-            <TiUserAddOutline
-                style={{ fontSize: 14, }} />
-            Add contact
-        </SubmitBtn>
-    </Form>;
+            <SubmitBtn
+                type="submit">
+                <TiUserAddOutline
+                    style={{ fontSize: 14, }} />
+                Add contact
+            </SubmitBtn>
+        </Form>;
     }
 };
 
