be05785f0155c702f937f8dda5ed7832557f5806
Oleksandr Romanchuk <novator0511@gmail.com>
2023-02-15 09:04:11 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,5 @@
-//========== hooks ==========
-import { useState, useEffect } from 'react';
-
-//========== helpers ==========
-import { nanoid } from 'nanoid';
-import { saveDataToLocalSt, loadDataFromLocalSt } from 'helpers/localStfunc';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
 
 //========== components ==========
 import { Section } from 'components/Section/Section';
@@ -15,48 +11,8 @@ import { ContactList } from 'components/ContactList/ContactList';
 //========== styles ==========
 import { PhonebookApp, Container, Title, Wrapper } from './App.styled';
 
-const LS_KEY = 'contacts';
-
 export function App() {
-	const [contacts, setContacts] = useState(
-		() => loadDataFromLocalSt(LS_KEY) ?? []
-	);
-	const [filter, setFilter] = useState('');
-
-	useEffect(() => {
-		saveDataToLocalSt(LS_KEY, contacts);
-	}, [contacts]);
-
-	const addNewContact = newContact => {
-		if (checkNewContact(newContact.name)) {
-			alert(`${newContact.name} is already in contacts.`);
-			return true;
-		}
-
-		newContact.id = nanoid();
-
-		setContacts(state => [newContact, ...state]);
-	};
-
-	const deleteContact = dataId => {
-		setContacts(contacts.filter(contact => contact.id !== dataId));
-	};
-
-	const setFilterWord = event => {
-		setFilter(event.target.value.trim());
-	};
-
-	const filteredContacts = () => {
-		const normalizeFilterWord = filter.toLowerCase();
-
-		return contacts.filter(({ name }) =>
-			name.toLowerCase().includes(normalizeFilterWord)
-		);
-	};
-
-	const checkNewContact = newName => {
-		return contacts.some(({ name }) => name === newName);
-	};
+	const contacts = useSelector(getContacts);
 
 	return (
 		<PhonebookApp>
@@ -64,7 +20,7 @@ export function App() {
 				<Title>Phonebook</Title>
 				<Wrapper>
 					<Section title="Form to add contacts">
-						<ContactForm getNewContactData={addNewContact} />
+						<ContactForm />
 					</Section>
 
 					<Section title="Contacts">
@@ -72,11 +28,8 @@ export function App() {
 							<Notification message="There are no contacts" />
 						) : (
 							<>
-								<Filter onChange={setFilterWord} />
-								<ContactList
-									contacts={filteredContacts()}
-									deleteContact={deleteContact}
-								/>
+								<Filter />
+								<ContactList />
 							</>
 						)}
 					</Section>
