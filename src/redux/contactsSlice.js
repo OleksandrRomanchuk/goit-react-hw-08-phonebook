941fb0c0bf3596ddd25fa41bf2457ab89e01643b
Oleksandr Romanchuk <novator0511@gmail.com>
2023-02-17 14:10:38 +0200

    refactoring Redux ToolKit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { nanoid } from 'nanoid';
+import { contactsInitialState } from 'initials/contactsInitialState';
+
+const persistConfig = {
+	key: 'contacts',
+	storage,
+	blacklist: ['persist/PERSIST'],
+};
+
+const contactsSlice = createSlice({
+	name: 'contacts',
+	initialState: contactsInitialState,
+	reducers: {
+		addContact: {
+			reducer(state, action) {
+				return { contacts: [...state.contacts, action.payload] };
+			},
+
+			prepare(contact) {
+				contact.id = nanoid(8);
+
+				return {
+					payload: contact,
+				};
+			},
+		},
+
+		deleteContact(state, action) {
+			state.contacts = state.contacts.filter(({ id }) => id !== action.payload);
+		},
+	},
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = persistReducer(
+	persistConfig,
+	contactsSlice.reducer
+);
