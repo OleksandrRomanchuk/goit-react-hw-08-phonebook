b16c5f26eee97c548b89ec0d459f6063931768a7
Oleksandr Romanchuk <novator0511@gmail.com>
2023-02-17 14:16:13 +0200

    add initial valuev for components / states

--- src/initials/initFormValues.jsx
+++
@@ -1,4 +0,0 @@
-export const initValues = {
-  name: '',
-  number: '',
-};
---
+++ src/initials/initialAddContactFormValues.js
@@ -0,0 +1,5 @@
+export const initialAddContactFormValues = {
+	contactName: '',
+	contactPhoneNumber: '',
+	contactsGroup: '',
+};
