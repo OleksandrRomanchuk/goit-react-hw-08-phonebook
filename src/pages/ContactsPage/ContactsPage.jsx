585e672e7b0d208ebbf28e0e76595a0a735ae3cf
Oleksandr Romanchuk <novator0511@gmail.com>
2023-02-26 21:19:47 +0200

    change ContactsPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,31 +2,27 @@ import { useSelector } from 'react-redux';
 import { getContacts } from 'redux/selectors';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Section } from 'components/Section/Section';
-import { Filter } from 'components/Filter/Filter';
-import { ContactList } from 'components/ContactList/ContactList';
-import { Notification } from 'components/Notification/Notification';
+import Section from 'components/Section/Section';
+import ContactList from 'components/ContactList/ContactList';
+import Notification from 'components/Notification/Notification';
+
+import { LeftContainer, RigthContainer } from './ContactsPage.styled';
 
 const ContactsPage = () => {
 	const contacts = useSelector(getContacts);
 	const areContacts = Boolean(contacts.length);
 
 	return (
-		<Section title="Contacts">
-			<div>
+		<Section>
+			<LeftContainer>
 				{!areContacts && <Notification message="There are no contacts yet." />}
-				{areContacts && (
-					<>
-						<Filter />
-						<ContactList />
-					</>
-				)}
-			</div>
-			<div>
+				{areContacts && <ContactList />}
+			</LeftContainer>
+			<RigthContainer>
 				<Suspense>
 					<Outlet />
 				</Suspense>
-			</div>
+			</RigthContainer>
 		</Section>
 	);
 };
---
+++ src/pages/ContactsPage/ContactsPage.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LeftContainer = styled.div`
+	width: 40%;
+`;
+
+export const RigthContainer = styled.div`
+	width: 60%;
+`;
