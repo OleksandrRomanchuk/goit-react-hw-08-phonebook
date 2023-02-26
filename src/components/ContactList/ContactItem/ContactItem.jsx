b0fe967a2653fa1c1ee304467178a67f0338036d
Oleksandr Romanchuk <novator0511@gmail.com>
2023-02-26 21:20:24 +0200

    update ContactItem

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,41 +1,60 @@
-//========== helpers ==========
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-
-//========== components ==========
+import { useLocation } from 'react-router-dom';
 import { TiUserDeleteOutline } from 'react-icons/ti';
-
-//========== actions ==========
+import { CgDetailsMore } from 'react-icons/cg';
+// import { BiEditAlt } from 'react-icons/bi';
 import { deleteContact } from 'redux/contactsSlice';
-
-//========== styles ==========
 import {
 	ContactWrapper,
 	ContactName,
 	PhoneNumber,
+	BtnList,
 	DeleteBtn,
+	DetailBtn,
+	// EditBtn,
 } from './ContactItem.styled';
 
-export const ContactItem = ({
+const ContactItem = ({
 	contact: { contactName, contactPhoneNumber, id },
 	...allyProps
 }) => {
+	const location = useLocation();
 	const dispatch = useDispatch();
 
 	return (
 		<>
+			<ContactName>{contactName}</ContactName>
 			<ContactWrapper>
-				<ContactName>{contactName}</ContactName>
 				<PhoneNumber>{contactPhoneNumber}</PhoneNumber>
+				<BtnList>
+					{/* <li>
+						<EditBtn
+							type="button"
+							onClick={() => dispatch(deleteContact(id))}
+							title="Delete contact"
+							{...allyProps}
+						>
+							<BiEditAlt />
+						</EditBtn>
+					</li> */}
+					<li>
+						<DetailBtn to={id} state={{ from: location }} title="Show more details">
+							<CgDetailsMore />
+						</DetailBtn>
+					</li>
+					<li>
+						<DeleteBtn
+							type="button"
+							onClick={() => dispatch(deleteContact(id))}
+							title="Delete contact"
+							{...allyProps}
+						>
+							<TiUserDeleteOutline />
+						</DeleteBtn>
+					</li>
+				</BtnList>
 			</ContactWrapper>
-			<DeleteBtn
-				type="button"
-				onClick={() => dispatch(deleteContact(id))}
-				title="Delete contact"
-				{...allyProps}
-			>
-				<TiUserDeleteOutline />
-			</DeleteBtn>
 		</>
 	);
 };
@@ -44,5 +63,8 @@ ContactItem.propTypes = {
 	contact: PropTypes.shape({
 		contactName: PropTypes.string.isRequired,
 		contactPhoneNumber: PropTypes.string.isRequired,
+		id: PropTypes.string.isRequired,
 	}),
 };
+
+export default ContactItem;
--- src/components/ContactList/ContactItem/ContactItem.styled.jsx
+++ src/components/ContactList/ContactItem/ContactItem.styled.jsx
@@ -1,48 +1,90 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const ContactWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-
-    min-width: 155px;
-    max-width: 155px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
 `;
 
 export const ContactName = styled.p`
-    font-size: 14px;
-    text-align: left;
+	font-size: 14px;
 `;
 
 export const PhoneNumber = styled.p`
-    margin-top: 2px;
+	font-size: 12px;
+	text-align: right;
+`;
 
-    font-size: 12px;
-    color: #ffffff72;
+export const BtnList = styled.ul`
+	display: flex;
+	gap: 10px;
+
+	margin-left: auto;
 `;
 
 export const DeleteBtn = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-    width: 20px;
-    height: 20px;
+	width: 20px;
+	height: 20px;
 
-    font-size: 14px;
+	font-size: 14px;
 
-    border-radius: 50%;
-    border: none;
-    outline: none;
+	border-radius: 50%;
+	border: none;
+	outline: none;
 
-    opacity: 0.4;
+	opacity: 0.4;
 
-    transition: opacity 300ms ease, background-color 300ms ease, color 300ms ease, box-shadow 300ms ease;
+	transition: opacity 300ms ease, background-color 300ms ease, color 300ms ease,
+		box-shadow 300ms ease;
 
-    :hover, :focus {
-        color: #ffffff;
+	:hover,
+	:focus {
+		color: #ffffff;
 
-        opacity: 1;
-        background-color: #ff4500;
-        box-shadow: 0px 0px 5px 2px #ff4500;
-    }
+		opacity: 1;
+		background-color: #ff4500;
+		box-shadow: 0px 0px 5px 2px #ff4500;
+	}
+`;
+
+export const DetailBtn = styled(Link)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	width: 20px;
+	height: 20px;
+
+	font-size: 14px;
+
+	border-radius: 50%;
+	border: none;
+	outline: none;
+
+	opacity: 0.4;
+
+	transition: opacity 300ms ease, background-color 300ms ease, color 300ms ease,
+		box-shadow 300ms ease;
+
+	:hover,
+	:focus {
+		color: #ffffff;
+
+		opacity: 1;
+		background-color: blue;
+		box-shadow: 0px 0px 5px 2px blue;
+	}
+`;
+
+export const EditBtn = styled(DeleteBtn)`
+	:hover,
+	:focus {
+		background-color: green;
+		box-shadow: 0px 0px 5px 2px green;
+	}
 `;
