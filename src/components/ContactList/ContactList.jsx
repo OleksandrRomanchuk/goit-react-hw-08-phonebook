cbe78b4289a432eb50a12d3f5334cd3a72568176
Oleksandr Romanchuk <novator0511@gmail.com>
2023-02-26 21:20:43 +0200

    update ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,12 @@
 import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
-import { Link, useLocation } from 'react-router-dom';
-import { ContactItem } from 'components/ContactList/ContactItem/ContactItem';
-import { Notification } from 'components/Notification/Notification';
-
-//========== styles ==========
+import ContactItem from 'components/ContactList/ContactItem/ContactItem';
+import Notification from 'components/Notification/Notification';
 import { List, ListItem } from './ContactList.styled';
 
-export const ContactList = () => {
+const ContactList = () => {
 	const contacts = useSelector(getContacts);
 	const filter = useSelector(getFilter);
-	const location = useLocation();
 	const filteredContacts = contacts.filter(({ contactName }) =>
 		contactName.toLowerCase().includes(filter)
 	);
@@ -20,9 +16,6 @@ export const ContactList = () => {
 		return (
 			<ListItem key={contact.id}>
 				<ContactItem contact={contact} aria-label="Delete contact" />
-				<Link to={contact.id} state={{ from: location }} title="Show more details">
-					Show details
-				</Link>
 			</ListItem>
 		);
 	});
@@ -36,3 +29,5 @@ export const ContactList = () => {
 		</>
 	);
 };
+
+export default ContactList;
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,29 +1,42 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const List = styled.ul`
-    min-width: 200px;
-    min-height: 200px;
-    max-height: 200px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
 
-    margin-top: 20px;
+	max-height: calc(100vh - 62px * 2);
+
+	overflow-y: scroll;
+
+	::-webkit-scrollbar {
+		width: 4px;
+	}
+
+	::-webkit-scrollbar-thumb {
+		background-color: #80808054;
+		border-radius: 2px;
+	}
+	/* margin-top: 20px;
     padding: 0 5px;
 
     text-align: left;
 
-    overflow-y: scroll;
+    
 
-    ::-webkit-scrollbar {
-        width: 4px;
-    };
-
-    ::-webkit-scrollbar-thumb {
-        background-color: #80808054;
-        border-radius: 2px;
-    };
+     */
 `;
 
 export const ListItem = styled.li`
-    display: flex;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	padding: 10px 6px;
+
+	border: 1px solid black;
+	border-radius: 3px;
+	/* display: flex;
     gap: 6px;
     justify-content: center;
     align-items: center;
@@ -37,6 +50,5 @@ export const ListItem = styled.li`
 
     :not(:first-child) {
         margin-top: 10px;
-    }
-
+    } */
 `;
