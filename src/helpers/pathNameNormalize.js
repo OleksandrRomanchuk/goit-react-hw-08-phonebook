d63eefc8cae6a3e60c3d981440dbcaa2c9f135fd
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-04 12:56:06 +0200

    Update pathNameNormalize.js

--- src/helpers/pathNameNormalize.js
+++ src/helpers/pathNameNormalize.js
@@ -1,8 +1,5 @@
 export const pathNameNormalize = group => {
 	const kebabCasePath = group.split(' ').join('-');
-	// console.log('kebabCasePath: ', kebabCasePath);
-
-	// console.log(kebabCasePath.charAt(0).toLowerCase());
 
 	return kebabCasePath.charAt(0).toLowerCase() + kebabCasePath.slice(1);
 };
