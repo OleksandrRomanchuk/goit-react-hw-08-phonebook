3ade8b2d39151f5ac8fbb382aee44b37569db51d
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-04 12:53:15 +0200

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import Container from 'components/Container/Container';
 import Filter from 'components/Filter/Filter';
 import AppNavigation from 'components/AppNavigation/AppNavigation';
+import { useLocation } from 'react-router-dom';
 import { NavBar, Logo } from './Header.styled';
 
 const Header = () => {
+	const { pathname } = useLocation();
+	const onGroupsPage = Boolean(pathname.includes('/my-groups'));
+
 	return (
 		<NavBar>
 			<Container layout="flex">
 				<Logo to="/my-contacts">Phonebook</Logo>
-				<Filter />
+				{!onGroupsPage && <Filter />}
+
 				<AppNavigation />
 			</Container>
 		</NavBar>
