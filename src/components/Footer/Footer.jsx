a74cac3ef24ea72f49f386f686cab1d0ee0676b3
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-04 12:52:23 +0200

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,48 @@
 import Container from 'components/Container/Container';
-import { FooterSection, Text } from './Footer.styled';
+import {
+	FooterSection,
+	Wrapper,
+	Text,
+	Link,
+	SubLink,
+	HelpersList,
+	ListItem,
+} from './Footer.styled';
 
 const Footer = () => {
 	return (
 		<FooterSection>
 			<Container layout="flex">
-				<Text>Developed by: ТРАЛІ_ВАЛІ ТРА_ТА_ТА</Text>
+				<Wrapper>
+					<Text>Developed by:</Text>
+					<div>
+						<Link
+							href="https://github.com/OleksandrRomanchuk"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Oleksandr Romanchuk
+						</Link>
+						<SubLink
+							href="https://m.goit.global/ua/new/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							GoIt Full Stack Online student
+						</SubLink>
+					</div>
+				</Wrapper>
+				<Wrapper>
+					<Text>Thanks for the help best:</Text>
+					<HelpersList>
+						<ListItem>
+							<p>blended-group-2, GoIt FSON 62;</p>
+						</ListItem>
+						<ListItem>
+							<p>Mentor.</p>
+						</ListItem>
+					</HelpersList>
+				</Wrapper>
 			</Container>
 		</FooterSection>
 	);
--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -3,10 +3,65 @@ import styled from 'styled-components';
 export const FooterSection = styled.footer`
 	flex: 0 0 auto;
 
-	border-top: 1px solid black;
+	padding: 11px 0;
+
+	background-color: rgb(7, 7, 34);
+	border-top: 1px solid #ffffffa4;
+
+	font-size: 12px;
+`;
+
+export const Wrapper = styled.div`
+	display: flex;
+	gap: 10px;
 `;
 
 export const Text = styled.p`
 	margin: 0 auto;
-	padding: 15px 0;
+
+	font-size: 14px;
+	font-weight: 400;
+	color: rgba(42, 153, 159, 1);
+`;
+
+export const Link = styled.a`
+	line-height: 16.1px;
+
+	transition: color 300ms ease-in-out;
+
+	:hover,
+	:focus {
+		color: rgba(241, 178, 17, 1);
+	}
+`;
+
+export const SubLink = styled(Link)`
+	font-size: 10px;
+	font-weight: 400;
+	color: #ffffffa4;
+`;
+
+export const HelpersList = styled.ul``;
+
+export const ListItem = styled.li`
+	position: relative;
+
+	padding-left: 18px;
+
+	::before {
+		content: '';
+		position: absolute;
+		left: 5px;
+		top: calc(50% - 3px);
+
+		width: 4px;
+		height: 4px;
+
+		border-radius: 50%;
+		background-color: rgba(232, 121, 17, 1);
+	}
+
+	:not(:first-child) {
+		margin-top: 3px;
+	}
 `;
