d01a7589c6e9e94716a6cd0aa1e9a52dfb428173
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-04 12:56:26 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,44 +1,37 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectContacts } from 'redux/selectors';
-import { getContactPropertyValues } from 'helpers/getContactPropertyValues';
+import { selectGroups } from 'redux/selectors';
 import { pathNameNormalize } from 'helpers/pathNameNormalize';
 
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 const ContactsPage = lazy(() => import('pages/ContactsPage/ContactsPage'));
-const ContactsSubPage = lazy(() =>
-	import('pages/ContactsSubPage/ContactsSubPage')
+const ContactDetailPage = lazy(() =>
+	import('pages/ContactDetailPage/ContactDetailPage')
 );
-const ContactsGroupPage = lazy(() =>
-	import('pages/ContactsGroupPage/ContactsGroupPage')
-);
+const EditPage = lazy(() => import('pages/EditPage/EditPage'));
+const ContactsGroupPage = lazy(() => import('pages/GroupsPage/GroupsPage'));
 const GroupContactList = lazy(() =>
 	import('components/GroupContactList/GroupContactList')
 );
 
 const App = () => {
-	const myGroups = getContactPropertyValues(
-		useSelector,
-		selectContacts,
-		'contactsGroup'
-	);
+	const myGroups = useSelector(selectGroups);
 
-	const myGroupsRoutes = myGroups.map(groupName => {
-		const normalizedGroupName = pathNameNormalize(groupName);
+	const myGroupsRoutes = myGroups.map(group => {
+		const normalizedGroupName = pathNameNormalize(group);
+
 		return (
 			<Route
-				key={groupName}
+				key={group}
 				path={
 					normalizedGroupName === '---'
 						? 'contacts-without-group'
 						: normalizedGroupName
 				}
-				element={<GroupContactList group={groupName} />}
-			>
-				<Route path=":id" element={<ContactsSubPage />} />
-			</Route>
+				element={<GroupContactList group={group} />}
+			/>
 		);
 	});
 
@@ -47,12 +40,17 @@ const App = () => {
 			<Route path="/" element={<SharedLayout />}>
 				<Route index element={<HomePage />} />
 				<Route path="my-contacts" element={<ContactsPage />}>
-					<Route path=":id" element={<ContactsSubPage />} />
+					<Route path=":id" element={<ContactDetailPage />} />
+					<Route path="edit/:id" element={<EditPage />} />
 				</Route>
 				<Route path="my-groups" element={<ContactsGroupPage />}>
 					{myGroupsRoutes}
 				</Route>
-				<Route path="/my-contacts/:id/details" element={<ContactsSubPage />} />
+				<Route
+					path="/my-contacts/:id/details"
+					element={<ContactDetailPage group="all" />}
+				/>
+				<Route path="/my-contacts/edit/:id" element={<EditPage />} />
 				<Route path="*" element={<ContactsPage />} />
 			</Route>
 		</Routes>
