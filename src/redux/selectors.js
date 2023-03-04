dd5f9f190e7dceae05331c668204c11512f79da1
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-04 12:49:37 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,40 @@
-export const selectContacts = state => state.contacts.contacts;
+import { createSelector } from '@reduxjs/toolkit';
 
-export const selectFilter = state => state.filter;
+const selectContacts = state => state.contacts;
+
+const selectFilter = state => state.filter;
+
+const selectIsLoading = state => state.contacts.isLoading;
+
+const selectError = state => state.contacts.error;
+
+const selectFilteredContacts = state => {
+	const contacts = selectContacts(state);
+	const filter = selectFilter(state);
+
+	const filteredContacts = contacts.items.filter(({ name }) =>
+		name.toLowerCase().includes(filter)
+	);
+
+	return { ...state, contacts: { ...contacts, items: filteredContacts } };
+};
+
+const selectGroups = createSelector([selectContacts], ({ items }) => {
+	const valuesOfProperties = items
+		.map(({ group }) => group)
+		.filter((item, ind, arr) => ind === arr.indexOf(item))
+		.sort((a, b) => a.localeCompare(b));
+
+	valuesOfProperties.unshift('All');
+
+	return valuesOfProperties;
+});
+
+export {
+	selectContacts,
+	selectFilter,
+	selectIsLoading,
+	selectError,
+	selectFilteredContacts,
+	selectGroups,
+};
