46a6aa61b27923e0fec6f43274c77b89002ad685
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-05 12:38:57 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import { groupFilterReducer } from './groupFilterSlice';
 
 export const store = configureStore({
 	reducer: {
 		contacts: contactsReducer,
 		filter: filterReducer,
+		groupFilter: groupFilterReducer,
 	},
 });
