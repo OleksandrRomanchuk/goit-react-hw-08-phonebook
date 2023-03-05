9d66d5a2477cef253e4a955c0ee4c8139c2c3dea
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-05 12:39:55 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,5 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectGroups } from 'redux/selectors';
-import { pathNameNormalize } from 'helpers/pathNameNormalize';
 
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
@@ -12,29 +9,8 @@ const ContactDetailPage = lazy(() =>
 );
 const EditPage = lazy(() => import('pages/EditPage/EditPage'));
 const ContactsGroupPage = lazy(() => import('pages/GroupsPage/GroupsPage'));
-const GroupContactList = lazy(() =>
-	import('components/GroupContactList/GroupContactList')
-);
 
 const App = () => {
-	const myGroups = useSelector(selectGroups);
-
-	const myGroupsRoutes = myGroups.map(group => {
-		const normalizedGroupName = pathNameNormalize(group);
-
-		return (
-			<Route
-				key={group}
-				path={
-					normalizedGroupName === '---'
-						? 'contacts-without-group'
-						: normalizedGroupName
-				}
-				element={<GroupContactList group={group} />}
-			/>
-		);
-	});
-
 	return (
 		<Routes>
 			<Route path="/" element={<SharedLayout />}>
@@ -43,9 +19,7 @@ const App = () => {
 					<Route path=":id" element={<ContactDetailPage />} />
 					<Route path="edit/:id" element={<EditPage />} />
 				</Route>
-				<Route path="my-groups" element={<ContactsGroupPage />}>
-					{myGroupsRoutes}
-				</Route>
+				<Route path="my-groups" element={<ContactsGroupPage />} />
 				<Route
 					path="/my-contacts/:id/details"
 					element={<ContactDetailPage group="all" />}
