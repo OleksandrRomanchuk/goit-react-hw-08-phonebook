775285258cf14d3ac987c3050208b348e95afdd2
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-05 12:39:37 +0200

    Update ContactItem.jsx

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,24 +1,8 @@
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { TiUserDeleteOutline } from 'react-icons/ti';
-import { CgDetailsMore } from 'react-icons/cg';
-import { BiEditAlt } from 'react-icons/bi';
-import { deleteContact } from 'redux/operations';
-import {
-	ContactWrapper,
-	ContactName,
-	PhoneNumber,
-	BtnList,
-	DeleteBtn,
-	DetailBtn,
-	EditBtn,
-} from './ContactItem.styled';
+import BtnsList from 'components/BtnsList/BtnsList';
+import { ContactWrapper, ContactName, PhoneNumber } from './ContactItem.styled';
 
 const ContactItem = ({ contact: { name, phoneNumber, id } }) => {
-	const dispatch = useDispatch();
-	const location = useLocation();
-
 	return (
 		<>
 			<ContactWrapper>
@@ -26,31 +10,7 @@ const ContactItem = ({ contact: { name, phoneNumber, id } }) => {
 				<PhoneNumber>{phoneNumber}</PhoneNumber>
 			</ContactWrapper>
 			<ContactWrapper>
-				<BtnList>
-					<li>
-						<DetailBtn to={id} state={{ from: location }} title="Show more details">
-							<CgDetailsMore />
-						</DetailBtn>
-					</li>
-					<li>
-						<EditBtn
-							to={`edit/${id}`}
-							state={{ from: location }}
-							title="Edit contact"
-						>
-							<BiEditAlt />
-						</EditBtn>
-					</li>
-					<li>
-						<DeleteBtn
-							type="button"
-							onClick={() => dispatch(deleteContact(id))}
-							title="Delete contact"
-						>
-							<TiUserDeleteOutline />
-						</DeleteBtn>
-					</li>
-				</BtnList>
+				<BtnsList id={id} />
 			</ContactWrapper>
 		</>
 	);
