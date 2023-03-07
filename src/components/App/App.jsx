a1b6301d6e4b15ecfce5f7f0914f3e72484133e5
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-07 16:24:02 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,29 +2,20 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 const ContactsPage = lazy(() => import('pages/ContactsPage/ContactsPage'));
 const ContactDetailPage = lazy(() =>
 	import('pages/ContactDetailPage/ContactDetailPage')
 );
 const EditPage = lazy(() => import('pages/EditPage/EditPage'));
-const ContactsGroupPage = lazy(() => import('pages/GroupsPage/GroupsPage'));
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<SharedLayout />}>
-				<Route index element={<HomePage />} />
 				<Route path="my-contacts" element={<ContactsPage />}>
 					<Route path=":id" element={<ContactDetailPage />} />
 					<Route path="edit/:id" element={<EditPage />} />
 				</Route>
-				<Route path="my-groups" element={<ContactsGroupPage />} />
-				<Route
-					path="/my-contacts/:id/details"
-					element={<ContactDetailPage group="all" />}
-				/>
-				<Route path="/my-contacts/edit/:id" element={<EditPage />} />
 				<Route path="*" element={<ContactsPage />} />
 			</Route>
 		</Routes>
