01f299d613ef21a0a750c7fec2fcd537cd22b71a
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-07 16:24:13 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
 import { initialAddContactFormValues } from 'initials/initialAddContactFormValues';
 import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectGroups } from 'redux/selectors';
+import { useDispatch } from 'react-redux';
 import { addContact } from 'redux/operations';
 import { TiUserAddOutline } from 'react-icons/ti';
 import {
@@ -14,13 +13,8 @@ import {
 import {
 	Form,
 	PositioningWrapperMain,
-	PositioningGruopWrapper,
 	Label,
-	GroupLabel,
-	GroupInput,
 	Input,
-	GroupSelect,
-	GroupSpan,
 	SubmitBtn,
 } from './ContactForm.styled';
 
@@ -30,38 +24,13 @@ const ContactForm = ({ toggleModal }) => {
 			loadDataFromLocalSt(localStorageFormValuesKey) ?? initialAddContactFormValues
 	);
 	const dispatch = useDispatch();
-	const myGroups = useSelector(selectGroups);
 
 	useEffect(() => {
 		saveDataToLocalSt(localStorageFormValuesKey, formValues);
 	}, [formValues]);
 
 	const handleInputChange = ({ target: { name, value } }) => {
-		switch (name) {
-			case 'contactsGroupList':
-				return (
-					formValues['group'] !== '---' &&
-					setFormValues(prevState => ({ ...prevState, group: value }))
-				);
-			case 'linkedin':
-				return setFormValues(prevState => ({
-					...prevState,
-					socialLinks: { ...prevState.socialLinks, [name]: value },
-				}));
-			case 'facebook':
-				return setFormValues(prevState => ({
-					...prevState,
-					socialLinks: { ...prevState.socialLinks, [name]: value },
-				}));
-			case 'telegram':
-				return setFormValues(prevState => ({
-					...prevState,
-					socialLinks: { ...prevState.socialLinks, [name]: value },
-				}));
-
-			default:
-				return setFormValues(prevState => ({ ...prevState, [name]: value }));
-		}
+		return setFormValues(prevState => ({ ...prevState, [name]: value }));
 	};
 
 	const onFormSubmit = async event => {
@@ -85,13 +54,7 @@ const ContactForm = ({ toggleModal }) => {
 		}
 	};
 
-	const {
-		name,
-		phoneNumber,
-		group,
-		email,
-		socialLinks: { linkedin = '', facebook = '', telegram = '' },
-	} = formValues;
+	const { name, number } = formValues;
 
 	return (
 		<Form onSubmit={onFormSubmit}>
@@ -114,84 +77,15 @@ const ContactForm = ({ toggleModal }) => {
 					<Input
 						onChange={handleInputChange}
 						type="tel"
-						name="phoneNumber"
+						name="number"
 						pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
 						title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-						value={phoneNumber}
+						value={number}
 						placeholder="+380 ....."
 						required
 					/>
 				</Label>
-				<Label>
-					Email:
-					<Input
-						onChange={handleInputChange}
-						type="email"
-						name="email"
-						value={email}
-						placeholder="some_body@....."
-					/>
-				</Label>
 			</PositioningWrapperMain>
-			<PositioningWrapperMain>
-				<Label>
-					Linkedin:
-					<Input
-						onChange={handleInputChange}
-						type="url"
-						name="linkedin"
-						value={linkedin}
-						placeholder="https://www.linkedin.com/in/....."
-					/>
-				</Label>
-				<Label>
-					Facebook:
-					<Input
-						onChange={handleInputChange}
-						type="url"
-						name="facebook"
-						value={facebook}
-						placeholder="https://www.facebook.com/....."
-					/>
-				</Label>
-				<Label>
-					Telegram:
-					<Input
-						onChange={handleInputChange}
-						type="url"
-						name="telegram"
-						value={telegram}
-						placeholder="https://t.me/....."
-					/>
-				</Label>
-			</PositioningWrapperMain>
-			<PositioningGruopWrapper>
-				<GroupLabel>
-					Enter new group name:
-					<GroupInput
-						onChange={handleInputChange}
-						type="text"
-						name="group"
-						value={group}
-						placeholder=""
-					/>
-				</GroupLabel>
-				<GroupSpan>or</GroupSpan>
-				<GroupLabel>
-					choose from the list:
-					<GroupSelect
-						onChange={handleInputChange}
-						name="contactsGroupList"
-						disabled={formValues.group && formValues.group !== '---' ? true : false}
-					>
-						{myGroups.map(group => (
-							<option key={group} value={group}>
-								{group}
-							</option>
-						))}
-					</GroupSelect>
-				</GroupLabel>
-			</PositioningGruopWrapper>
 			<SubmitBtn type="submit">
 				<TiUserAddOutline style={{ fontSize: 18 }} />
 				Add contact
