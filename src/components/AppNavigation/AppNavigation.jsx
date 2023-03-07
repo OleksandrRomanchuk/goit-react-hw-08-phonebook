f90dca9b083be9962a9161370729b39ee09c3818
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-07 16:24:05 +0200

    Update AppNavigation.jsx

--- src/components/AppNavigation/AppNavigation.jsx
+++ src/components/AppNavigation/AppNavigation.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
 
-import { AppNav, List, PageLink } from './AppNavigation.styled';
+import { AppNav, PageLink } from './AppNavigation.styled';
 
 const AppNavigation = () => {
 	const { pathname } = useLocation();
@@ -8,14 +8,7 @@ const AppNavigation = () => {
 
 	return (
 		<AppNav ongroupsPage={ongroupsPage}>
-			<List>
-				<li>
-					<PageLink to="/my-contacts">My contacts</PageLink>
-				</li>
-				<li>
-					<PageLink to="/my-groups">My groups</PageLink>
-				</li>
-			</List>
+			<PageLink to="/my-contacts">My contacts</PageLink>
 		</AppNav>
 	);
 };
