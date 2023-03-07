59034b1f4fb57ad9504efd0c38ab5bed5e1da640
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-07 16:24:39 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,30 +4,16 @@ import Footer from 'components/Footer/Footer';
 import ContactForm from 'components/ContactForm/ContactForm';
 import Modal from 'components/Modal/Modal';
 import { Suspense } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
-import { Body, Main, HomePageMain } from './SharedLayout.styled';
+import { Body, Main } from './SharedLayout.styled';
 
 const SharedLayout = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
-	const { pathname } = useLocation();
-	const onHomePage = pathname === '/';
 
 	const toggleModal = () => {
 		setIsModalOpen(prevState => !prevState);
 	};
-
-	if (onHomePage) {
-		return (
-			<Body>
-				<HomePageMain>
-					<Suspense>
-						<Outlet />
-					</Suspense>
-				</HomePageMain>
-			</Body>
-		);
-	}
 
 	return (
 		<Body>
