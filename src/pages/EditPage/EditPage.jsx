f786522ea2b6ebb496b425eb9ed5eaadb989b1d8
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-10 17:09:15 +0200

    Update EditPage.jsx

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,23 +1,21 @@
+import FormField from 'shared/components/FormField/FormFeild';
+import Button from 'shared/components/Button/Button';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectCurrentContact } from 'redux/contacts/contactSelectors';
-import { selectFilter } from 'redux/filter/filterSelectors';
-import { useParams, useNavigate } from 'react-router-dom';
-import { editContact } from 'redux/contacts/contactsOperations';
-import { fetchContactById } from 'redux/contacts/contactsOperations';
+import { selectCurrentContact } from 'redux/contacts/selectors';
+import { selectFilter } from 'redux/filter/selectors';
+import { useNavigate } from 'react-router-dom';
+import { editContact } from 'redux/contacts/operations';
+import { fields } from './fields';
 
 import {
 	EditForm,
 	PositioningWrapperMain,
-	Label,
-	Input,
-	SubmitBtn,
-} from 'shared/components/ContactForm/ContactForm.styled';
+} from 'modules/ContactForm/ContactForm.styled';
 
 const EditPage = () => {
 	const currentContact = useSelector(selectCurrentContact);
 	const [formValues, setFormValues] = useState(null);
-	const { id: itemId } = useParams();
 	const filter = useSelector(selectFilter);
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
@@ -25,10 +23,6 @@ const EditPage = () => {
 	useEffect(() => {
 		setFormValues(currentContact);
 	}, [currentContact]);
-
-	useEffect(() => {
-		dispatch(fetchContactById(itemId));
-	}, [dispatch, itemId]);
 
 	if (!currentContact) return;
 
@@ -54,37 +48,23 @@ const EditPage = () => {
 
 	return (
 		<>
-			{formValues && !filter && (
+			{formValues && (
 				<EditForm onSubmit={onFormSubmit}>
 					<PositioningWrapperMain>
-						<Label>
-							Name:
-							<Input
-								onChange={handleInputChange}
-								type="text"
-								name="name"
-								pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-								title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-								value={formValues.name}
-								placeholder="Gregor ....."
-								required
-							/>
-						</Label>
-						<Label>
-							Phone number:
-							<Input
-								onChange={handleInputChange}
-								type="tel"
-								name="number"
-								pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-								title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-								value={formValues.number}
-								placeholder="+380 ....."
-								required
-							/>
-						</Label>
+						<FormField
+							handleChange={handleInputChange}
+							pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+							value={formValues.name}
+							{...fields.name}
+						/>
+						<FormField
+							handleChange={handleInputChange}
+							pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+							value={formValues.number}
+							{...fields.number}
+						/>
 					</PositioningWrapperMain>
-					<SubmitBtn type="submit">Save changes</SubmitBtn>
+					<Button type="submit" text="Save changes" title="Save changes" />
 				</EditForm>
 			)}
 		</>
