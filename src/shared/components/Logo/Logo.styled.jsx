e721e62b76ecbe1dc2d750f30bd6c3aeea65cb2d
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-10 17:00:49 +0200

    Create Logo.styled.jsx

---
+++ src/shared/components/Logo/Logo.styled.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const LogoLink = styled(Link)`
+	align-self: flex-start;
+
+	padding: 10px 10px;
+
+	font-size: 30px;
+	font-weight: 900;
+	width: -webkit-min-content;
+	width: -moz-min-content;
+	width: min-content;
+	background: linear-gradient(
+		220deg,
+		rgba(250, 0, 46, 1) 0%,
+		rgba(250, 0, 46, 1) 19%,
+		transparent 19%,
+		transparent 20%,
+		rgba(232, 121, 17, 1) 20%,
+		rgba(232, 121, 17, 1) 39%,
+		transparent 39%,
+		transparent 40%,
+		rgba(241, 178, 17, 1) 40%,
+		rgba(241, 178, 17, 1) 59%,
+		transparent 59%,
+		transparent 60%,
+		rgba(42, 153, 159, 1) 60%,
+		rgba(42, 153, 159, 1) 79%,
+		transparent 79%,
+		transparent 80%,
+		rgba(24, 107, 184, 1) 80%,
+		rgba(24, 107, 184, 1) 100%
+	);
+	background-clip: text;
+	-webkit-background-clip: text;
+	color: transparent;
+`;
