450b535e0d322490718f7baefef05c9d382f8f6a
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-10 17:06:06 +0200

    Create LoggedOutNavBar.jsx

---
+++ src/modules/LoggedOutNavBar/LoggedOutNavBar.jsx
@@ -0,0 +1,13 @@
+import Logo from 'shared/components/Logo/Logo';
+import AppNavigation from 'shared/components/AppNavigation/AppNavigation';
+
+const LoggedOutNavBar = () => {
+	return (
+		<>
+			<Logo />
+			<AppNavigation />
+		</>
+	);
+};
+
+export default LoggedOutNavBar;
