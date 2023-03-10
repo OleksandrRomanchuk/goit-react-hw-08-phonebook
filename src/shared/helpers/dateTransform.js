9f1e378a6e7eb4a55ab41b66754b22980a9d682b
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-10 16:59:22 +0200

    removed unnecessary components

--- src/shared/helpers/dateTransform.js
+++
@@ -1,7 +0,0 @@
-export const dateTransform = dateTime => {
-	const devide = dateTime.split('T');
-	const date = devide[0].split('-').reverse().join('.');
-	const time = devide[1].slice(0, 5);
-
-	return `${time} ${date}`;
-};
